f69768b2970d7bb65102c0bc42438d37c6047f03
iodry <65801534+iodry@users.noreply.github.com>
2020-06-17 23:39:42 -0400

    Add smooth player camera

---
+++ SafeShop/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 finalPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, finalPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothPosition;
+    }
+
+}

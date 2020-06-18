42b91ae091554b49fbd769f6d1ae69c52726b00d
iodry <65801534+iodry@users.noreply.github.com>
2020-06-17 22:40:04 -0400

    Add player and joystick control
    Imported Standard assets 3rd person
    Imported Joystick asset

---
+++ SafeShop/Assets/Scripts/Gameplay/ThirdPersonInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ThirdPersonInput : MonoBehaviour
+{
+    public FixedJoystick fixedJoystick;
+    private ThirdPersonUserControl control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GetComponent<ThirdPersonUserControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        control.hInput = fixedJoystick.Horizontal;
+        control.vInput = fixedJoystick.Vertical;
+    }
+}

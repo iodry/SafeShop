d47a432a90df76328b82111d05735ffbe7de6ea6
iodry <65801534+iodry@users.noreply.github.com>
2020-06-20 23:14:48 -0400

    Player personalization
    add player personalization
    add items library for inventory

---
+++ SafeShop/Assets/Scripts/Inventory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int slotNumber = 2;
+    public List<IInventoryItem> mItems = new List<IInventoryItem>();
+    public event EventHandler<InventoryEventArgs> ItemAdded;
+    public event EventHandler<InventoryEventArgs> ItemRemoved;
+
+    public void AddItem(IInventoryItem item)
+    {
+       // Debug.Log("Items " + mItems.Count);
+        if (mItems.Count < slotNumber)
+        {
+            Collider collider = (item as MonoBehaviour).GetComponent<Collider>();
+            if (collider.enabled)
+            {
+                collider.enabled = false;
+                mItems.Add(item);
+                item.OnPickup();
+                //Debug.Log("Inventory");
+                if (ItemAdded != null)
+                {
+                    ItemAdded(this, new InventoryEventArgs(item));
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
